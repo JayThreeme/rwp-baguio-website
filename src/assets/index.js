9b5f251695b7a2547a8f3c0382a9f3b06d7591b2
JayThreeme <jayjucarjr@gmail.com>
2024-11-17 15:31:58 +0800

    update: assets index

---
+++ src/assets/index.js
@@ -0,0 +1,34 @@
+//logo
+import rwp_black from "./logo/rwp_black";
+import rwp from "./logo/rwp";
+import rwpbaguio from "./logo/rwpbaguio";
+import rwpbaguio_black from "./logo/rwpbaguio_black";
+
+//socials
+import facbook from "./socials/facbook";
+import instagram from "./socials/instagram";
+import mail from "./socials/mail";
+import telegram from "./socials/telegram";
+
+//stock
+import philippines_circle from "./stock/philippines_circle";
+import philippine_rec from "./stock/philippine_rec";
+import runner from "./stock/runner";
+import running_blue from "./stock/running_blue";
+import running from "./stock/running";
+
+export default {
+  rwp,
+  rwp_black,
+  rwpbaguio,
+  rwpbaguio_black,
+  facbook,
+  instagram,
+  mail,
+  telegram,
+  philippines_circle,
+  philippine_rec,
+  runner,
+  running_blue,
+  running,
+};
